fcd4f6e89ef0967535d7fb027008d27f91ad6b54
mohan.bharani <mohan.bharani@gmail.com>
2020-03-31 08:45:22 +0530

    First Commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+
+    serverId: number = 10;
+    serverStatus: string = 'Active Server';
+    isServer: boolean = false;
+    
+    getServerStatus(){
+        return this.serverStatus;
+    }
+}
